6e67e00167a67a26c6806b91d70a48a87871bdee
CEP <galcera@hotmail.com>
2018-12-14 12:34:35 +0100

    about us modificado

--- views/about_us.php
+++ views/about_us.php
@@ -8,12 +8,24 @@
 <?php startblock('principal') ?>
     <div class="main">
         <div class="titulo">
-            <h1>Adventur3</h1>
+            <h1>Welcome to Adventur3</h1>
         </div>
 
-        <div>
-            <p class="abstract">fyjftybhjfjj</p>
+        <div class="abstract">
+        <h3>About the team</h3>
+        </br>
+        <p>Somos Adventur3, un equipo de jovenes programadores que trabaja actualmente para CEPSoft Consulting.
+        </br></br>NeverForget es uno de nuestros proyectos mas ambiciosos que pretende revolucionar el concepto actual de visita de interes aumentado.
+        NeverForget le da al usuario la posibilidad de hacer un recorrido por la ciudad de Barcelona visitando los sitios mas relevantes durante la Guerra Civil Española.
+        Toda la experiéncia se basa en sumir al usuario en una historia adaptada al contexto historico de la epoca.
+        </br></br>El objetivo es dar a conocer la historia des de un punto de vista turistico, lúdico y cultural. Para ello hemos creado un recorrido lineal por la ciudad con cinco paradas obligatorias donde el usuario deberá resolver un enigma en cada una de ellas.
+        Al final del recorrido el usuario 
+        </p>
+        </br>
+        <h3>Members</h3>
         </div>
+
+        
 
         <div class="container">
 
@@ -26,9 +38,9 @@
                             <p class="card-text"></p>
                             <div class="card-footer justify-content-center">
                                 <ul>
-                                    <li><a href="" target="_blank"><i class="fa fa-twitter iconAbout"></i></a></li>
-                                    <li><a href="" target="_blank"><i class="fa fa-linkedin iconAbout"></i></a></li>
-                                    <li><a href="" target="_blank"><i class="fa fa-instagram iconAbout"></i></a></li>    
+                                    <li><a href="https://twitter.com/Retsebdn" target="_blank"><i class="fa fa-twitter iconAbout"></i></a></li>
+                                    <li><a href="https://www.linkedin.com/in/esther-galdeano/" target="_blank"><i class="fa fa-linkedin iconAbout"></i></a></li>
+                                    <li><a href="https://www.instagram.com/retse_str/" target="_blank"><i class="fa fa-instagram iconAbout"></i></a></li>    
                                 </ul> 
                             </div>
                         </div>
@@ -75,7 +87,7 @@
                             <p class="card-text"></p>
                             <div class="card-footer justify-content-center">
                                 <ul>
-                                    <li><a href="" target="_blank"><i class="fa fa-twitter iconAbout"></i></a></li>
+                                    <li><a href="https://twitter.com/JordiPare26" target="_blank"><i class="fa fa-twitter iconAbout"></i></a></li>
                                     <li><a href="" target="_blank"><i class="fa fa-linkedin iconAbout"></i></a></li>
                                     <li><a href="" target="_blank"><i class="fa fa-instagram iconAbout"></i></a></li>    
                                 </ul> 
@@ -88,12 +100,12 @@
                         <img class="card-img-top rounded-circle" src="../assets/img/avatar/avatar_JordiPe.png" alt="Card image cap">
                         <div class="card-body">
                             <h5 class="card-title">Jordi Peña</h5>
-                            <p class="card-text"></p>
+                            <p class="card-text"> </p>
                             <div class="card-footer justify-content-center">
                                 <ul>
-                                    <li><a href="" target="_blank"><i class="fa fa-twitter iconAbout"></i></a></li>
-                                    <li><a href="" target="_blank"><i class="fa fa-linkedin iconAbout"></i></a></li>
-                                    <li><a href="" target="_blank"><i class="fa fa-instagram iconAbout"></i></a></li>    
+                                    <li><a href="https://twitter.com/Jordipem" target="_blank"><i class="fa fa-twitter iconAbout"></i></a></li>
+                                    <li><a href="https://www.linkedin.com/in/jordi-pe%C3%B1a-811240177/" target="_blank"><i class="fa fa-linkedin iconAbout"></i></a></li>
+                                    <li><a href="https://www.instagram.com/jordi_xt/" target="_blank"><i class="fa fa-instagram iconAbout"></i></a></li>    
                                 </ul> 
                             </div>
                         </div>
