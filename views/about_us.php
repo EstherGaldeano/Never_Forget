038d7d7c134b17ab2daf80c4b9154ab6471bb17b
Francesc Retamal <franrere.6.97@gmail.com>
2018-12-14 13:02:37 +0100

    about us abstract

--- views/about_us.php
+++ views/about_us.php
@@ -13,72 +13,25 @@
 
         <div class="abstract">
         <h3>About the team</h3>
-        </br>
+        <br>
         <p>Somos Adventur3, un equipo de jovenes programadores que trabaja actualmente para CEPSoft Consulting.
-        </br></br>NeverForget es uno de nuestros proyectos mas ambiciosos que pretende revolucionar el concepto actual de visita de interes aumentado.
-        NeverForget le da al usuario la posibilidad de hacer un recorrido por la ciudad de Barcelona visitando los sitios mas relevantes durante la Guerra Civil Española.
+        <br><br>NeverForget es uno de nuestros proyectos mas ambiciosos que pretende revolucionar el concepto actual de 
+        visita de interes aumentado.
+        NeverForget le da al usuario la posibilidad de hacer un recorrido por la ciudad de Barcelona visitando los sitios mas 
+        relevantes durante la Guerra Civil Española.
         Toda la experiéncia se basa en sumir al usuario en una historia adaptada al contexto historico de la epoca.
-        </br></br>El objetivo es dar a conocer la historia des de un punto de vista turistico, lúdico y cultural. Para ello hemos creado un recorrido lineal por la ciudad con cinco paradas obligatorias donde el usuario deberá resolver un enigma en cada una de ellas.
-        Al final del recorrido el usuario 
+        <br><br>El objetivo es dar a conocer la historia des de un punto de vista turistico, lúdico y cultural. Para ello hemos 
+        creado un recorrido lineal por la ciudad con cinco paradas obligatorias donde el usuario deberá resolver un enigma en cada una de ellas.
+        
         </p>
-        </br>
+        <br>
         <h3>Members</h3>
         </div>
-
         
 
         <div class="container">
 
             <div class="row justify-content-center">
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="card">
-                        <img class="card-img-top rounded-circle" src="../assets/img/avatar/avatar_Esther.png" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Esther Galdeano</h5>
-                            <p class="card-text"></p>
-                            <div class="card-footer justify-content-center">
-                                <ul>
-                                    <li><a href="https://twitter.com/Retsebdn" target="_blank"><i class="fa fa-twitter iconAbout"></i></a></li>
-                                    <li><a href="https://www.linkedin.com/in/esther-galdeano/" target="_blank"><i class="fa fa-linkedin iconAbout"></i></a></li>
-                                    <li><a href="https://www.instagram.com/retse_str/" target="_blank"><i class="fa fa-instagram iconAbout"></i></a></li>    
-                                </ul> 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="card">
-                        <img class="card-img-top rounded-circle" src="../assets/img/avatar/avatar_Diego.png" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Diego Rodriguez</h5>
-                            <p class="card-text"></p>
-                            <div class="card-footer justify-content-center">
-                                <ul>
-                                    <li><a href="" target="_blank"><i class="fa fa-twitter iconAbout"></i></a></li>
-                                    <li><a href="" target="_blank"><i class="fa fa-linkedin iconAbout"></i></a></li>
-                                    <li><a href="" target="_blank"><i class="fa fa-instagram iconAbout"></i></a></li>    
-                                </ul> 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="card">
-                        <img class="card-img-top rounded-circle" src="../assets/img/avatar/avatar_Francesc.png" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Francesc Retamal</h5>
-                            <p class="card-text">Pequeña descipción de cada uno</p>
-                            <div class="card-footer justify-content-center">
-                                <ul>
-                                    <li><a href="https://twitter.com/Fran97Chuk?lang=es" target="_blank"><i class="fa fa-twitter iconAbout"></i></a></li>
-                                    <li><a href="https://www.linkedin.com/in/francesc-retamal-requena/" target="_blank"><i class="fa fa-linkedin iconAbout"></i></a></li>
-                                    <li><a href="https://twitter.com/Fran97Chuk?lang=es" target="_blank"><i class="fa fa-instagram iconAbout"></i></a></li>    
-                                </ul>   
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <div class="col-md-4 col-sm-6 col-xs-12">
                     <div class="card">
                         <img class="card-img-top rounded-circle" src="../assets/img/avatar/avatar_JordiPa.png" alt="Card image cap" style="background-color: white">
@@ -95,6 +48,7 @@
                         </div>
                     </div>
                 </div>
+
                 <div class="col-md-4 col-sm-6 col-xs-12">
                     <div class="card">
                         <img class="card-img-top rounded-circle" src="../assets/img/avatar/avatar_JordiPe.png" alt="Card image cap">
@@ -107,6 +61,61 @@
                                     <li><a href="https://www.linkedin.com/in/jordi-pe%C3%B1a-811240177/" target="_blank"><i class="fa fa-linkedin iconAbout"></i></a></li>
                                     <li><a href="https://www.instagram.com/jordi_xt/" target="_blank"><i class="fa fa-instagram iconAbout"></i></a></li>    
                                 </ul> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="card">
+                        <img class="card-img-top rounded-circle" src="../assets/img/avatar/avatar_Esther.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Esther Galdeano</h5>
+                            <p class="card-text"></p>
+                            <div class="card-footer justify-content-center">
+                                <ul>
+                                    <li><a href="https://twitter.com/Retsebdn" target="_blank"><i class="fa fa-twitter iconAbout"></i></a></li>
+                                    <li><a href="https://www.linkedin.com/in/esther-galdeano/" target="_blank"><i class="fa fa-linkedin iconAbout"></i></a></li>
+                                    <li><a href="https://www.instagram.com/retse_str/" target="_blank"><i class="fa fa-instagram iconAbout"></i></a></li>    
+                                </ul> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="card">
+                        <img class="card-img-top rounded-circle" src="../assets/img/avatar/avatar_Diego.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Diego Rodriguez</h5>
+                            <p class="card-text"></p>
+                            <div class="card-footer justify-content-center">
+                                <ul>
+                                    <li><a href="" target="_blank"><i class="fa fa-twitter iconAbout"></i></a></li>
+                                    <li><a href="" target="_blank"><i class="fa fa-linkedin iconAbout"></i></a></li>
+                                    <li><a href="" target="_blank"><i class="fa fa-instagram iconAbout"></i></a></li>    
+                                </ul> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="card">
+                        <img class="card-img-top rounded-circle" src="../assets/img/avatar/avatar_Francesc.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Francesc Retamal</h5>
+                            <p class="card-text">Pequeña descipción de cada uno</p>
+                            <div class="card-footer justify-content-center">
+                                <ul>
+                                    <li><a href="https://twitter.com/Fran97Chuk?lang=es" target="_blank"><i class="fa fa-twitter iconAbout"></i></a></li>
+                                    <li><a href="https://www.linkedin.com/in/francesc-retamal-requena/" target="_blank"><i class="fa fa-linkedin iconAbout"></i></a></li>
+                                    <li><a href="https://twitter.com/Fran97Chuk?lang=es" target="_blank"><i class="fa fa-instagram iconAbout"></i></a></li>    
+                                </ul>   
+                                
                             </div>
                         </div>
                     </div>
